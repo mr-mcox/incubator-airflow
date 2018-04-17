6dc4e88e9b89e1c95d4541dce85257d7e40d45b3
Matthew Cox <mr.mcox@gmail.com>
2018-04-17 16:08:23 -0500

    Adding some debugging

--- airflow/utils/log/gcs_task_handler.py
+++ airflow/utils/log/gcs_task_handler.py
@@ -7,9 +7,9 @@
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
-# 
+#
 #   http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -41,11 +41,15 @@ class GCSTaskHandler(FileTaskHandler, LoggingMixin):
 
     def _build_hook(self):
         remote_conn_id = configuration.conf.get('core', 'REMOTE_LOG_CONN_ID')
+        self.log.debug('Got remote connection')
         try:
             from airflow.contrib.hooks.gcs_hook import GoogleCloudStorageHook
-            return GoogleCloudStorageHook(
+            self.log.debug('Imported library')
+            gcs_hook = GoogleCloudStorageHook(
                 google_cloud_storage_conn_id=remote_conn_id
             )
+            self.log.debug('Got hook')
+            return gcs_hook
         except Exception as e:
             self.log.error(
                 'Could not create a GoogleCloudStorageHook with connection id '
@@ -117,7 +121,9 @@ class GCSTaskHandler(FileTaskHandler, LoggingMixin):
             log = '*** Unable to read remote log from {}\n*** {}\n\n'.format(
                 remote_loc, str(e))
             self.log.error(log)
-            log += super(GCSTaskHandler, self)._read(ti, try_number)
+            super_read = super(GCSTaskHandler, self)._read(ti, try_number)
+            self.log.debug(super_read)
+            log += super_read[0]
 
         return log, {'end_of_log': True}
 
